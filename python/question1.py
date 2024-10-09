3a94260bf00eaec6dbae79f37422a27af0e245d0
iqbalmaulanaputra <iqbalputra090303@gmail.com>
2024-10-09 12:22:58 +0700

    question

---
+++ python/question1.py
@@ -0,0 +1,11 @@
+# omplete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+#step 1
+# def solution(string, ending):
+#     return string.endswith(ending)
+
+#step 2
+solution = str.endswith
+
+print(solution('abc', 'bc'))  
+print(solution('abc', 'd'))
